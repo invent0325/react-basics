9585dc750a315e91b8e2d2fa4196cb21fd89ba56
LAWRENCE PINTO <lpinto1204@gmail.com>
2022-07-20 20:24:25 +0530

    Content Code

--- src/Content.js
+++ src/Content.js
@@ -23,8 +23,9 @@ const Content = () => {
 
   const handleCheck = (id) => {
     const listItems = items.map((item) =>
-      item.id === id ? { ...item, checked: !item.checked } : item);
-      setItems(listItems);
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setItems(listItems);
     console.log("Key : " + id);
   };
 
