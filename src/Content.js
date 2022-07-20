017d5ad4516724cd4b9818f7a6733f6d783531c0
LAWRENCE PINTO <lpinto1204@gmail.com>
2022-07-20 11:08:51 +0530

    react code

--- src/Content.js
+++ src/Content.js
@@ -1,20 +1,37 @@
-import React from 'react'
+import React from "react";
 
 const Content = () => {
+  const handleNameChange = () => {
+    const names = ["Jill", "Jane", "John"];
+    const check = Math.floor(Math.random() * 3);
+    return names[check];
+  };
 
-    const handleNameChange = () => {
-        const names = ['Jill', 'Jane', 'John'];
-        const check = Math.floor(Math.random() * 3);
-        return names[check];
-    }
+  const handleClick = () => {
+    console.log("handleClick");
+  };
+
+  const handleClick2 = (name) => {
+    console.log(`${name} was clicked`);
+  };
+
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText);
+  };
+
+
 
   return (
     <main>
-        <p>
-            Hello {handleNameChange()}!
-        </p>
+      <p>Hello {handleNameChange()}!</p>
+      <br />
+      <button onClick={handleClick}>Click Me!</button>
+      <br />
+      <button onClick={() => handleClick2("Dave")}>Display Name!</button>
+      <br />
+      <button onClick={(e) => handleClick3(e)}>Event Details!</button>
     </main>
-  )
-}
+  );
+};
 
-export default Content
+export default Content;
